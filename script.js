5e5684388e6e79d66e7bc371a23ce9f35e59785d
Ronishan <roni@example.com>
2026-01-03 13:05:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// let movie;
+// movie="Anbae shivam"
+// console.log(movie)
+
+
+// let thala=100
+// let thalapathy=100
+// let thalathalapathy=thala+thalapathy
+// console.log(thalathalapathy)
+
+
+
+// let lionisanimal=true
+// let crowisbird=true
+// let dragonisalien=false
+
+// console.log(dragonisalien)
+
+
+// let chocolate=10;
+
+// chocolate=chocolate+2;
+// console.log(chocolate)
+
+
+// let number=230
+// let newnumber=String(number)
+// console.log(typeof newnumber)
+
+
+// let name="rajini"
+// let newname=Boolean(name)
+// console.log(newname)
+
+// console.log(parseFloat(45.6248).toFixed(1))
+
+
+
+// function calculate (hour) {
+//     hours=Number.(N)
+//     if hours<
+    
+// }
+ 
+// let weight=prompt("enter your weight")
+// // let height=prompt("enter your height")
+// console.log("your weight is"+weight+" "+"kg")
+function revealname(){
+    document.getElementById("heroname").innerText="My name is roni"
+}
